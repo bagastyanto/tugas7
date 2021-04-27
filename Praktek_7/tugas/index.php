3273f0bb23a1829ef957907d8e9a09624c3d15c2
bagastyanto <79794446+bagastyanto@users.noreply.github.com>
2021-04-27 22:00:52 +0700

    Add files via upload

---
+++ Praktek_7/tugas/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+<style media="screen">
+	body {
+		background: yellow;
+	}
+</style>
+</head>
+<body>
+	<!--menampilkan tulisan dengan format heading 2-->
+	<h2 align="center"> Login </h2>
+	<!--membuat form dengan method post-->
+	<form method="post" action="ceklogin.php">
+		<!--membuat tabel untuk field inputan dan button-->
+		<table align="center">
+		<tr>
+			<td>Username</td>
+			<td><input type="text" name="username"></td>
+		</tr>
+		<tr>
+			<td>Password</td>
+			<td><input type="password" name="password"></td>
+		</tr>
+		<tr>
+			<td><button type="submit" name="Login">Login</button></td>
+		</tr>
+		</table>
+	</form>
+
+</body>
+</html>
